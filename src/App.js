ad95c6ec4b8c59006f506364013e94b983faafce
raheelahmed99 <raheelahmedkumbhar@gmail.com>
2024-02-14 11:51:04 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.css';
+import Header from './Compoonents/Common/Header/Header';
+import SignIn from './Compoonents/Features/SignIn/SignIn';
+import Sell from './Compoonents/Features/SellPage/Sell';
+import Footer from "./Compoonents/Common/Footer/Footer";
+import ProductCategoury from "./Compoonents/Features/ProductCategoury/ProductCategoury";
+import ShowNavbarFooter from "./Compoonents/ShowNavbar&Footer/ShowNavbarFooter";
+
+// import Fetch from "./Compoonents/Features/Fetch/Fetch";
+// import { useGlobalContextHook } from "./Compoonents/Contaxt/Contaxt";
+
+function App() {
+  // const name = useGlobalContextHook();
+ 
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <ShowNavbarFooter>
+      <Header />
+    </ShowNavbarFooter>
+    
+    <Routes>
+      <Route path="/" element={ <ProductCategoury />} />
+          
+      <Route path="SignIn" element={ <SignIn /> } />
+      <Route path="Sell" element={ <Sell /> } />
+    </Routes>
+   
+   
+    <Footer/>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
